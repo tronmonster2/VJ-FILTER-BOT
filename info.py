8495061c33888aa9e9a6794b1f32978a96e0c751
tronmonster2 <159607389+tronmonster2@users.noreply.github.com>
2024-02-23 19:35:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Cluster0:D9QUEgzUlg19BFxh@cluster0.5dzvhrt.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster 0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
